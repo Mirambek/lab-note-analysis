4ee8959a4f7606ea06767427736af3371dbb62ef
MirambekNagashbekov <n.mirambek@gmail.com>
2021-05-09 23:15:16 +0600

    🔨 configured local storage

--- src/shared/store/store.ts
+++ src/shared/store/store.ts
@@ -1,7 +1,14 @@
 import { Container } from 'aurelia-framework';
-import { Store } from 'aurelia-store';
+import { Store,localStorageMiddleware, MiddlewarePlacement, rehydrateFromLocalStorage } from 'aurelia-store';
 import { State } from '../types/state';
+  
+
 
 const store: Store<State> = Container.instance.get<State>(Store);
+const key = 'note-entry-key';
+store.registerMiddleware(localStorageMiddleware, MiddlewarePlacement.After, { key });
+store.registerAction('Rehydrate', rehydrateFromLocalStorage);
+store.dispatch(rehydrateFromLocalStorage, key);
+
 
 export default store;
